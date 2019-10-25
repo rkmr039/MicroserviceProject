2873c32cb599550bc4e675991b72b3f18fbb3a27
rkmr039 <rkmr039@gmail.com>
2019-10-25 13:38:15 +0530

    WebClientAdded

--- BankWebClient/src/main/java/com/rishab/BankWebClientApplication.java
+++ BankWebClient/src/main/java/com/rishab/BankWebClientApplication.java
@@ -2,14 +2,14 @@ package com.rishab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class DiscoveryMasterApplication {
+@EnableEurekaClient
+public class BankWebClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryMasterApplication.class, args);
+		SpringApplication.run(BankWebClientApplication.class, args);
 	}
 
 }
