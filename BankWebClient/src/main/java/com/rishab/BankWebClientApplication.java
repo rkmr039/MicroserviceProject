f0ae16328e9eb538bc75dea5f9a7939c8110acff
rkmr039 <rkmr039@gmail.com>
2019-10-25 18:16:57 +0530

    .

--- BankWebClient/src/main/java/com/rishab/BankWebClientApplication.java
+++ BankWebClient/src/main/java/com/rishab/BankWebClientApplication.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class BankWebClientApplication {
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankWebClientApplication.class, args);
