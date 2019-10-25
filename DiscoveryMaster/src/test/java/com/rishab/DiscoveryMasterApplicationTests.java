c5ababebb171dcdc22262045d8d04995ff9275bc
rkmr039 <rkmr039@gmail.com>
2019-10-25 13:36:40 +0530

    DiscoveryMaster added

---
+++ DiscoveryMaster/src/test/java/com/rishab/DiscoveryMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+" # MicroserviceProject" 
+
+Capability To Generate 
+
+
+RESTful 
+Spring MVC
+Hibernate
+Logging
+Mockito
+Jenkins 
+SonarQube
+
+
+Java HTML5 CSS3 JavaScript 
